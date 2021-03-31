d0b820f320bd0f8370e4f15259327adf6de688db
Josandonas <josandonas@outlook.com>
2021-03-31 02:13:13 -0400

    Subindo arquivos de Exemplo C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Program > {} EstruDoPrograma
+using System;
+using EstruDoPrograma.Exemplos;
+namespace EstruDoPrograma{
+    class Program{
+        static void Main(string[] args){
+            var s= new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            var valor= new LacoBoasVindas();
+            valor.lacoBoas(7);
+        }
+    }
+}
