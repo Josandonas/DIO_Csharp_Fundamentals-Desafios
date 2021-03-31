e956da60ca30e86bcd6242ba708d9cb3031c9662
Josandonas <josandonas@outlook.com>
2021-03-31 17:15:48 -0400

    codigo de divisao em procedimento ainda

--- Program.cs
+++ Program.cs
@@ -4,15 +4,18 @@ using EstruDoPrograma.Exemplos;
 namespace EstruDoPrograma{
     class Program{
         static void Main(string[] args){
-            var s= new Pilha();
-            s.Empilha(1);
-            s.Empilha(10);
-            s.Empilha(100);
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
-            var valor= new LacoBoasVindas();
-            valor.lacoBoas(7);
+            // var s= new Pilha();
+            // var valor= new LacoBoasVindas();
+            var div= new divisao();
+            div.dividindo();
+            // valor.lacoBoas(7);
+
+            // s.Empilha(1);
+            // s.Empilha(10);
+            // s.Empilha(100);
+            // Console.WriteLine(s.Desempilha());
+            // Console.WriteLine(s.Desempilha());
+            
         }
     }
 }
