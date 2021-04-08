16bb0b1253447478a355cbeacebfc3dd9045ccc9
Josandonas <josandonas@outlook.com>
2021-04-08 10:37:37 -0400

    Desafio das coxinhas

--- DesafiosIntro2/CoxinhaDeBueno.cs
+++ DesafiosIntro2/CoxinhaDeBueno.cs
@@ -1,5 +1,11 @@
+using System;
 namespace EstruDoPrograma.DesafiosIntro2{
     public class CoxinhaDeBueno{
-        
+        public void coxinha(){
+            string[] line = Console.ReadLine().Split(" ");
+            double coxinhas = int.Parse(line[0]), pessoas = int.Parse(line[1]);
+            double media = coxinhas / pessoas;
+            Console.WriteLine(media.ToString("N2"));
+        }
     }
 }
